7b4a1883ac8c9a828322b2ba9cb97e33e7fcdbf4
celesc16 <celechoquevillca2004@gmail.com>
2026-05-29 02:18:03 -0300

    The backend  is being restructured.

--- backend/apps/music/urls.py
+++ backend/apps/music/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from ....apps.music import views
 
 urlpatterns = [
     path('search/',                          views.song_search,    name='song-search'),
